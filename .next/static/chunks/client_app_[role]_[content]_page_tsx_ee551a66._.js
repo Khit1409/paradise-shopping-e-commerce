f55906ba2d45c77a3e361b5cd08d99a3dc5fe7a0
Your Name <you@example.com>
2025-11-12 14:38:34 +0700

    Remove node_modules and .next from tracking

--- .next/static/chunks/client_app_[role]_[content]_page_tsx_ee551a66._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/client_14777824._.js",
-  "static/chunks/_b2369d7f._.js"
-],
-    source: "dynamic"
-});
