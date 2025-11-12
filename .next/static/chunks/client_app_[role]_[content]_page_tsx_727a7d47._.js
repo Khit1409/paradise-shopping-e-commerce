cea07f9b090626ea11a65c37a1bca2e52f843c83
Your Name <you@example.com>
2025-11-12 14:38:34 +0700

    Remove node_modules and .next from tracking

--- .next/static/chunks/client_app_[role]_[content]_page_tsx_727a7d47._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/client_e7a23b68._.js",
-  "static/chunks/_a957c161._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/client_app_[role]_single-product_page_tsx_ee551a66._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/client_b10f3475._.js"
-],
-    source: "dynamic"
-});
